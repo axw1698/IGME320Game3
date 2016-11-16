6baa0d61152cc18079f40bc0f336971117f63cd2
axw1698 <axw1698@g.rit.edu>
2016-11-15 19:53:04 -0500

    update key and door open
    Need to add environment later

---
+++ OuttaTimeGame3/Assets/Script/doorOpen.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorOpen : MonoBehaviour {
+
+    public GameObject thisGO;
+    bool openDoor = false;
+    public bool withKey = false;
+    float yRotationRight = 2.0f;
+    float yRotationLeft = 180.0f;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        print("FPSController");
+        if (this.thisGO.name == "doorRight")
+        {
+            float doorRightDis = Vector3.Distance(transform.position, thisGO.transform.position);
+            if (doorRightDis < 5)
+            {
+                print("In distance");
+                if (withKey == true)
+                {
+                    if (Input.GetKey(KeyCode.E))
+                    {
+                        openDoor = true;
+                    }
+                }
+            }
+        }
+        if (this.thisGO.name == "doorLeft")
+        {
+            float doorLeftDis = Vector3.Distance(transform.position, thisGO.transform.position);
+            if (doorLeftDis < 5)
+            {
+                print("In distance");
+                if (withKey == true)
+                {
+                    if (Input.GetKey(KeyCode.E))
+                    {
+                        openDoor = true;
+                    }
+                }
+            }
+        }
+        if (openDoor == true)
+        {
+
+            if (thisGO.name == "doorRight")
+            {
+                if (thisGO.transform.eulerAngles.y <= 90.0f)
+                {
+                    //print("Open door" + thisGO.transform.eulerAngles.y);
+
+                    //print("Open door" + thisGO.transform.rotation.y);
+                    yRotationRight += 2;
+                    //thisGO.transform.Rotate(0, 2, 0);
+                    transform.eulerAngles = new Vector3(0, yRotationRight, 0);
+                }
+            }
+            if (thisGO.name == "doorLeft")
+            {
+                if (thisGO.transform.eulerAngles.y >= 90.0f)
+                {
+                    //print("Open door" + thisGO.transform.eulerAngles.y);
+                    yRotationLeft -= 2;
+                    //thisGO.transform.Rotate(0, 2, 0);
+                    transform.eulerAngles = new Vector3(0, yRotationLeft, 0);
+                }
+            }
+            //else if(thisGO.transform.rotation.y >= 90)
+            //{
+            //    print("Stop open door");
+            //    openDoor = false;
+            //    return;
+            //    //thisGO.transform.Rotate(0, 0, 0);
+
+            //}
+
+        }
+    }
+}
--- OuttaTimeGame3/Assets/Script/key.cs
+++ OuttaTimeGame3/Assets/Script/key.cs
@@ -8,12 +8,20 @@ public class key : MonoBehaviour {
     GameObject player;
     GameObject keyGO;
     GameObject questionMark;
+    GameObject doorLeft;
+    GameObject doorRight;
+
 	// Use this for initialization
 	void Start () {
-        player = GameObject.Find("FPSController");
-        keyGO = GameObject.Find("keyImg");
-        questionMark = GameObject.Find("questionMark");
-        keyGO.SetActive(false);
+        if (thisGO.name == "Chest")
+        {
+            player = GameObject.Find("FPSController");
+            keyGO = GameObject.Find("keyImg");
+            questionMark = GameObject.Find("questionMark");
+            keyGO.SetActive(false);
+        }
+        doorLeft = GameObject.Find("doorLeft");
+        doorRight = GameObject.Find("doorRight");
 
 
     }
@@ -23,18 +31,21 @@ public class key : MonoBehaviour {
 
         if (thisGO.name == "Chest")
         {
-            float getDistance = Vector3.Distance(player.transform.position, thisGO.transform.position);
+            float chestDistance = Vector3.Distance(player.transform.position, thisGO.transform.position);
 
-            if (getDistance < 7)
+            if (chestDistance < 7)
             {
-                if (Input.GetKey(KeyCode.E))
+                if (Input.GetKeyUp(KeyCode.E))
                 {
                     DestroyObject(thisGO);
                     keyGO.SetActive(true);
                     questionMark.SetActive(false);
+                    GameObject.Find("doorRight").GetComponent<doorOpen>().withKey = true;
+                    GameObject.Find("doorLeft").GetComponent<doorOpen>().withKey = true;
+
                 }
             }
-            
+
         }
 
     }
