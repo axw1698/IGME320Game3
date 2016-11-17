fa7af33e34073c38ee37ca4e13c9940d965cc68d
emc7541 <emc7541@g.rit.edu>
2016-11-17 13:33:52 -0500

    Added pathfinding
    Skeleton is going in loopy loops

--- OuttaTimeGame3/Assets/Script/Enemy/Seeker.cs
+++ OuttaTimeGame3/Assets/Script/Enemy/Seeker.cs
@@ -6,8 +6,8 @@ public class Seeker : Vehicle {
     //Should it move?
     public bool followPath;
 
-    //The movement points
-    public Vector3[] movementPoints;
+    // Waypoints
+    public Waypoint[] path;
 
 
     //Also not sure why I need this
@@ -16,18 +16,19 @@ public class Seeker : Vehicle {
 
     //Not sure why I need this
     //Where I was - list index
-    private int prevPoint;
+    //private int prevPoint;
 
     //Where I'm going - list index
-    private int nextPoint = 0;
+    private int currentNode = 0;
 
     public GameObject seekerTarget;
+    public Waypoint waypointTarget;
 
 	//WEIGHTING!!!!!!!!!!!!
-	public float seekWeight=75.0f;
-	public float alignWeight=20.0f;
-	public float cohesionWeight=20.0f;
-	public float seperationWeight=20.0f;
+	public float seekWeight = 75.0f;
+	public float alignWeight = 20.0f;
+	public float cohesionWeight = 20.0f;
+	public float seperationWeight = 20.0f;
 	public float avoidWeight = 120.0f;
 
 
@@ -39,10 +40,6 @@ public class Seeker : Vehicle {
 	public float safeDistance = 10.0f;
 
 
-
-
-
-
 	// Call Inherited Start and then do our own
 	override public void Start () {
 		base.Start();
@@ -51,17 +48,25 @@ public class Seeker : Vehicle {
         maxSpeed = 10f;
 
         isCat = false;
-
         followPath = true;
 
         startingPosition = gameObject.transform.position;
-        movementPoints = new Vector3[6];
-
+        
         ultimateForce = Vector3.zero;
 
-	}
+        // Create the path
+        path = new Waypoint[7];
+        path[0] = new Waypoint(GameObject.Find("WP0").transform.position, GameObject.Find("WP1").transform.position);
+        path[1] = new Waypoint(GameObject.Find("WP1").transform.position, GameObject.Find("WP2").transform.position);
+        path[2] = new Waypoint(GameObject.Find("WP2").transform.position, GameObject.Find("WP3").transform.position);
+        path[3] = new Waypoint(GameObject.Find("WP3").transform.position, GameObject.Find("WP4").transform.position);
+        path[4] = new Waypoint(GameObject.Find("WP4").transform.position, GameObject.Find("WP5").transform.position);
+        path[5] = new Waypoint(GameObject.Find("WP5").transform.position, GameObject.Find("WP6").transform.position);
+        path[6] = new Waypoint(GameObject.Find("WP6").transform.position, GameObject.Find("WP0").transform.position);
+        
+    }
 
-	protected override void CalcSteeringForces()
+    protected override void CalcSteeringForces()
 	{
 		//reset the ultimate force
 		ultimateForce = Vector3.zero;
@@ -69,42 +74,63 @@ public class Seeker : Vehicle {
         //Am I set to follow a path? Does the paths have points?
         if (followPath)
         {
+            // Get the waypoint
+            waypointTarget = path[currentNode];
 
+            Vector3 steeringForce = Seek(waypointTarget.GetStart) * seekWeight;
 
-            //get the next movement point in the list
-            Vector3 temp = movementPoints[nextPoint];
+            // Limit the steering force
+            steeringForce = Vector3.ClampMagnitude(steeringForce, maxForce);
 
+            // Apply it to accerleration
+            ApplyForce(steeringForce);
 
-            //check distance between me and the movement point
-            temp = transform.position - temp;
+            // Reset
+            steeringForce = Vector3.zero;
 
-
-
-            //if I'm within units from the point, find the next point
-            //if I reach the end of the list, change movement point
-            //back to the beginning of the list
-            if (temp.magnitude <= 8)
+            // Get next waypoint
+            if (Vector3.Distance(transform.position, path[currentNode].GetStart) <= 3.0f)
             {
-                if (nextPoint == movementPoints.Length - 1)
+                if (currentNode == path.Length-1)
                 {
-                    nextPoint = 0;
+                    // Reset
+                    currentNode = 0;
                 }
-                else {
-                    nextPoint = nextPoint + 1;
+                else
+                {
+                    currentNode++;
                 }
             }
+            
+            
+            ////get the next movement point in the list
+            //Vector3 point = path[nextPoint].transform.position;
 
+            ////check distance between me and the movement point
+            //point = transform.position - point;
 
+            ////if I'm within units from the point, find the next point
+            ////if I reach the end of the list, change movement point
+            ////back to the beginning of the list
+            //if (point.magnitude <= 8)
+            //{
+            //    if (nextPoint == path.Length - 1)
+            //    {
+            //        nextPoint = 0;
+            //    }
+            //    else {
+            //        nextPoint = nextPoint + 1;
+            //    }
+            //}
 
-            //seek next determined spot
-            Vector3 followForce = Seek(movementPoints[nextPoint]);
+            ////seek next determined spot
+            //Vector3 followForce = Seek(path[nextPoint].transform.position);
 
-            //limiting the steering force
-            followForce = Vector3.ClampMagnitude(followForce, maxForce);
+            ////limiting the steering force
+            //followForce = Vector3.ClampMagnitude(followForce, maxForce);
 
-            //apply steering force to the acceleration (AppplyForce())
-            ApplyForce(followForce);
-
+            ////apply steering force to the acceleration (AppplyForce())
+            //ApplyForce(followForce);
 
         }
         else
@@ -113,19 +139,16 @@ public class Seeker : Vehicle {
         //get a seeking force (based on char movement - for now, just sek)
         Vector3 seekForce = Seek (seekerTarget.transform.position)*seekWeight;
 
-
-
 		//add that seeking force to alternate steering for
 		ultimateForce+=seekForce;
 
-
-
         //limiting the steering force
-        ultimateForce =Vector3.ClampMagnitude (ultimateForce,maxForce);
+        ultimateForce = Vector3.ClampMagnitude (ultimateForce,maxForce);
 
 		//apply steering force to the acceleration (AppplyForce())
 		ApplyForce (ultimateForce);
         }
 
 	}
+    
 }
---
+++ OuttaTimeGame3/Assets/Script/Enemy/Waypoint.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint
+{
+    // attributes
+    private Vector3 start, end;
+    private Vector3 segment;  // from point A to B
+    private Vector3 unit;
+
+    public Vector3 GetStart { get { return start; } }
+
+    public Vector3 GetEnd { get { return end; } }
+
+    // Constructor
+    public Waypoint(Vector3 startPoint, Vector3 endPoint)
+    {
+        start = startPoint;
+        end = endPoint;
+
+        segment = end - start;
+        unit = segment.normalized;
+    }
+
+    // Calculate the closest point
+    public Vector3 ClosestPoint (Vector3 point)
+    {
+        Debug.DrawLine(start, end, Color.cyan);  // debug
+
+        // Calculate the projection
+        float projection = Vector3.Dot(point - start, segment);
+
+        // Calculate the closest point
+        Vector3 closestPoint = start + unit * projection;
+
+        return closestPoint;
+    }
+}
