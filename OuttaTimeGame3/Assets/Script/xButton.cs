31fd82a6459b18912a5d285d8e28234552f8997f
Christopher Littlefield <cxl9645@g.rit.edu>
2016-11-19 22:25:01 -0500

    Rapid X
    Button indicator when being dragged has been added.

---
+++ OuttaTimeGame3/Assets/Script/xButton.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class xButton : MonoBehaviour {
+
+    float switchTime;
+    float frameRate;
+    SpriteRenderer sr;
+    public Sprite buttonUp;
+    public Sprite buttonDown;
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        switchTime = 0.0f;
+        frameRate = 0.0625f;
+
+        if(sr.sprite == null)
+        {
+            sr.sprite = buttonUp;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sr.sprite == buttonUp)
+        {
+            switchTime += Time.deltaTime;
+            if (switchTime > frameRate)
+            {
+                sr.sprite = buttonDown;
+                switchTime = 0.0f;
+            }
+        }
+
+        else
+        {
+            switchTime += Time.deltaTime;
+            if (switchTime > frameRate)
+            {
+                sr.sprite = buttonUp;
+                switchTime = 0.0f;
+            }
+        }
+    }
+}
